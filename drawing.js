0330b5ccae37c2316908ca00f1f3cb5a70fdf91a
karhuz <fragfreak@gmail.com>
2016-01-12 10:34:35 +0200

    added simple fps
    i can render something yay

--- drawing.js
+++ drawing.js
@@ -1,10 +1,46 @@
-var changeSmiley = False;
+
+//sick fps system
+var timeri = setInterval(TimerTick, 60.333);
+// ------
+
+var changeSmiley = false;
+
+var hymio = ":-)";
 
 
+//otetaan centteri canvasi
 var c = document.getElementById("canvas_center");
-var ctx = c.getContext("2d");
+
+
+
+
+// piirtamiseen helpottavia muuttujia
+
+var center_x = (c.width/2);
+var center_y = (c.height/2);
+
+
+
+
+function drawScreen() {
+	var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
-ctx.fillText("Moro",10,50);
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+
+var lol = Math.random();
+var lol2 = lol *200;
+ctx.fillText(":-)",center_x+lol2,center_y+lol2);
+}
+
+function clearScreen()
+{
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, c.width, c.height);
+}
+
+
+function TimerTick(){
+
+	clearScreen();
+	drawScreen();
+	//console.log("tik tik");
+}
